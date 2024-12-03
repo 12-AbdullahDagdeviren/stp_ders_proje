69c2a7e1615c42c6a4634f05e099d1dafb7dfab7
12-AbdullahDagdeviren <profilo12abdullahdagdeviren@gmail.com>
2024-12-03 11:36:52 +0300

    Add files via upload
    03.12.2024
    güne3cellendi

---
+++ amp12c_558_stp/stok_takip_programi/stok_takip_programi/Models/Musteri.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace stok_takip_programi.Models
+{
+    public class Musteri
+    {
+        public int Id { get; set; }
+        public string AdSoyad { get; set; }
+        public string FirmaAdi { get; set; }
+        public int Telefon { get; set; }
+        public string Mail { get; set; }
+        public string Adres { get; set; }
+
+        public virtual ICollection<Satis> Urunler { get; set; } = new HashSet<Satis>();
+
+    }
+}
---
+++ amp12c_558_stp/stok_takip_programi/stok_takip_programi/View/frmKategoriIslemleri.Designer.cs
@@ -0,0 +1,192 @@
+﻿namespace stok_takip_programi
+{
+    partial class frmKategoriIslemleri
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmKategoriIslemleri));
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnYeni = new System.Windows.Forms.Button();
+            this.ımageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.btnGuncelle = new System.Windows.Forms.Button();
+            this.ımageList4 = new System.Windows.Forms.ImageList(this.components);
+            this.btnSil = new System.Windows.Forms.Button();
+            this.ımageList3 = new System.Windows.Forms.ImageList(this.components);
+            this.txtKategoriAdi = new System.Windows.Forms.TextBox();
+            this.btnEkle = new System.Windows.Forms.Button();
+            this.ımageList2 = new System.Windows.Forms.ImageList(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.grdKategoriler = new System.Windows.Forms.DataGridView();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.grdKategoriler)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnYeni);
+            this.groupBox1.Controls.Add(this.btnGuncelle);
+            this.groupBox1.Controls.Add(this.btnSil);
+            this.groupBox1.Controls.Add(this.txtKategoriAdi);
+            this.groupBox1.Controls.Add(this.btnEkle);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(40, 30);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(321, 193);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Bilgi Girişi";
+            // 
+            // btnYeni
+            // 
+            this.btnYeni.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnYeni.ImageKey = "1771029.png";
+            this.btnYeni.ImageList = this.ımageList1;
+            this.btnYeni.Location = new System.Drawing.Point(213, 19);
+            this.btnYeni.Name = "btnYeni";
+            this.btnYeni.Size = new System.Drawing.Size(102, 37);
+            this.btnYeni.TabIndex = 6;
+            this.btnYeni.Text = "YENİ KAYIT";
+            this.btnYeni.UseVisualStyleBackColor = true;
+            // 
+            // ımageList1
+            // 
+            this.ımageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("ımageList1.ImageStream")));
+            this.ımageList1.TransparentColor = System.Drawing.Color.Transparent;
+            this.ımageList1.Images.SetKeyName(0, "1771029.png");
+            // 
+            // btnGuncelle
+            // 
+            this.btnGuncelle.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnGuncelle.ImageKey = "1689164.png";
+            this.btnGuncelle.ImageList = this.ımageList4;
+            this.btnGuncelle.Location = new System.Drawing.Point(213, 123);
+            this.btnGuncelle.Name = "btnGuncelle";
+            this.btnGuncelle.Size = new System.Drawing.Size(108, 40);
+            this.btnGuncelle.TabIndex = 4;
+            this.btnGuncelle.Text = "GÜNCELLE";
+            this.btnGuncelle.UseVisualStyleBackColor = true;
+            // 
+            // ımageList4
+            // 
+            this.ımageList4.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("ımageList4.ImageStream")));
+            this.ımageList4.TransparentColor = System.Drawing.Color.Transparent;
+            this.ımageList4.Images.SetKeyName(0, "1689164.png");
+            // 
+            // btnSil
+            // 
+            this.btnSil.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnSil.ImageKey = "484662.png";
+            this.btnSil.ImageList = this.ımageList3;
+            this.btnSil.Location = new System.Drawing.Point(120, 123);
+            this.btnSil.Name = "btnSil";
+            this.btnSil.Size = new System.Drawing.Size(87, 40);
+            this.btnSil.TabIndex = 3;
+            this.btnSil.Text = "SİL";
+            this.btnSil.UseVisualStyleBackColor = true;
+            // 
+            // ımageList3
+            // 
+            this.ımageList3.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("ımageList3.ImageStream")));
+            this.ımageList3.TransparentColor = System.Drawing.Color.Transparent;
+            this.ımageList3.Images.SetKeyName(0, "484662.png");
+            // 
+            // txtKategoriAdi
+            // 
+            this.txtKategoriAdi.Location = new System.Drawing.Point(94, 63);
+            this.txtKategoriAdi.Name = "txtKategoriAdi";
+            this.txtKategoriAdi.Size = new System.Drawing.Size(100, 20);
+            this.txtKategoriAdi.TabIndex = 5;
+            // 
+            // btnEkle
+            // 
+            this.btnEkle.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnEkle.ImageKey = "992651.png";
+            this.btnEkle.ImageList = this.ımageList2;
+            this.btnEkle.Location = new System.Drawing.Point(27, 123);
+            this.btnEkle.Name = "btnEkle";
+            this.btnEkle.Size = new System.Drawing.Size(87, 40);
+            this.btnEkle.TabIndex = 2;
+            this.btnEkle.Text = "EKLE";
+            this.btnEkle.UseVisualStyleBackColor = true;
+            // 
+            // ımageList2
+            // 
+            this.ımageList2.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("ımageList2.ImageStream")));
+            this.ımageList2.TransparentColor = System.Drawing.Color.Transparent;
+            this.ımageList2.Images.SetKeyName(0, "992651.png");
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(24, 70);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(67, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Kategori Adı:";
+            // 
+            // grdKategoriler
+            // 
+            this.grdKategoriler.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.grdKategoriler.Location = new System.Drawing.Point(40, 247);
+            this.grdKategoriler.Name = "grdKategoriler";
+            this.grdKategoriler.Size = new System.Drawing.Size(321, 153);
+            this.grdKategoriler.TabIndex = 1;
+            // 
+            // frmKategoriIslemleri
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.grdKategoriler);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "frmKategoriIslemleri";
+            this.Text = "frmKategoriIslemleri";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.grdKategoriler)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnGuncelle;
+        private System.Windows.Forms.Button btnSil;
+        private System.Windows.Forms.ImageList ımageList2;
+        private System.Windows.Forms.TextBox txtKategoriAdi;
+        private System.Windows.Forms.Button btnEkle;
+        private System.Windows.Forms.ImageList ımageList1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ImageList ımageList3;
+        private System.Windows.Forms.DataGridView grdKategoriler;
+        private System.Windows.Forms.Button btnYeni;
+        private System.Windows.Forms.ImageList ımageList4;
+    }
+}
+
